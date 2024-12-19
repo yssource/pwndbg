cc20bd6654f410f5afa955bb88bbfa5bcb5b862f
patryk4815 <patryk.sondej@grupawp.pl>
2024-12-19 16:42:42 +0100

    Add next part of adding kernel support for LLDB (#2640)
    * add kernel support for lldb
    
    * remove negative check
    
    * int0

--- pwndbg/commands/klookup.py
+++ pwndbg/commands/klookup.py
@@ -18,15 +18,19 @@ parser.add_argument("symbol", type=str, help="Address or symbol name to lookup")
 def klookup(symbol: str) -> None:
     ksyms = pwndbg.aglib.kernel.kallsyms.get()
     try:
-        symbol_addr = int(symbol)
-        for k, v in ksyms.items():
+        symbol_addr = int(symbol, 0)
+        for ksym, v in ksyms.items():
             if v[0] == symbol_addr:
-                print(message.success(f"{k} = {symbol_addr:#x}"))
+                print(message.success(f"{symbol_addr:#x} = {ksym}"))
                 return
         print(message.error(f"No symbol found at {symbol_addr:#x}"))
     except ValueError:
-        if symbol in ksyms:
-            addr = ksyms[symbol][0]
-            print(message.success(f"{symbol} = {addr:#x}"))
-        else:
+        found = False
+        for ksym, v in ksyms.items():
+            if symbol not in ksym:
+                continue
+            found = True
+            addr = v[0]
+            print(message.success(f"{addr:#x} = {ksym}"))
+        if not found:
             print(message.error(f"No symbol found for {symbol}"))
